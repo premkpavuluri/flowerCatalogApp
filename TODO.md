b3bfa66a91a2986f7b5212db94765f8d3d962cdc
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-06-27 15:21:16 +0530

    Implemented flowerCatalog handler.

--- TODO.md
+++ TODO.md
@@ -1,14 +1,14 @@
 # Flower Catalog App
 
 - **TODO:**
-  - [ ] Implement flowerCatalogHandler.
-    - [x] handler for home page.
-    - [ ] handler for flower catalogs.
   - [ ] Implement guestBookHandler.
   - [ ] Implement flowerCatalogHomePage
-  - [ ] Implement pages for flowers
+  - [ ] Implement html  pages for flowers
 
 - **DONE:**
+  - [x] Implement flowerCatalogHandler.
+    - [x] handler for home page.
+    - [x] handler for flower catalogs.
   - [x] Implement the server for flower catalog app.
     - [x] Implement parser.
     - [x] Implement response entity.
--- src/flowerCatalogHandler.js
+++ src/flowerCatalogHandler.js
@@ -3,11 +3,28 @@ const homePageHandler = (request, response) => {
   return true;
 };
 
+const catalogHandler = (request, response) => {
+  const { queryParams } = request;
+  const { name } = queryParams;
+
+  if (!name) {
+    response.send('Invalid flower catalog');
+    return true;
+  }
+
+  response.send(`${name}'s page`);
+  return true;
+};
+
 const flowerCatalogHandler = (request, response) => {
   const { uri } = request;
 
   if (uri === '/') {
     return homePageHandler(request, response);
+  }
+
+  if (uri === '/catalog') {
+    return catalogHandler(request, response);
   }
 };
 
