4464f38e17250a8e97962a6fb6bbcf2308f63b98
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-13 10:54:03 +0530

    Refactored app.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { startServer } = require('myserver');
 const { connectHandlers } = require('./src/app.js');
 
-const resource = {
+const appConfig = {
   resourceRoot: process.argv[2],
   dbPath: './db/comments.json'
 };
 
-const router = connectHandlers(resource);
-startServer(8000, router);
+const app = connectHandlers(appConfig);
+
+startServer(8000, app);
